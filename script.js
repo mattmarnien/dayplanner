84355e2bdbebfa48fd7f011c78b926153123750f
mgm215 <60325909+mgm215@users.noreply.github.com>
2020-02-14 15:39:18 -0500

    Finished scripting for styling past, present future, and corrected issue with local storage

--- script.js
+++ script.js
@@ -1,46 +1,87 @@
 
-var timeNow =  $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm"));
+var timeNow = $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm"));
 
 var saveButtons = $(".save");
-var hoursArr = ["8 am","9 am","10 am","11 am", "12 pm","1 pm","2 pm", " 3 pm", "4 pm", "5 pm", "6 pm"];
+var hoursArr = ["8", "9", "10", "11", "12", "1", "2", "3", "4", "5", "6"];
 var scheduleArr = [];
+var currentHour = moment().hour();
 
-    for (let i =0; i < hoursArr.length; i++){
-      var  newRowDiv = $("<div>");
-        newRowDiv.attr("class", "row w-100");
-        $("#body").append(newRowDiv);
-        // console.log("In the for loop");
+for (let i = 0; i < hoursArr.length; i++) {
+  var newRowDiv = $("<div>");
+  newRowDiv.attr("class", "row w-100");
+  $("#body").append(newRowDiv);
+  
+   var newTimeDiv = $("<div>");
+  var newEventSection = $("<section>");
+  var newSaveDiv = $("<div>");
+  newTimeDiv.attr("class", "col-1 time justify-center rounded-left pt-4");
+  newTimeDiv.attr("data", hoursArr[i])
+  newEventSection.attr("class", "col-10 event justify-center border-left border-right border-dark pt-4");
+  newEventSection.attr("dataId", hoursArr[i])
+  newEventSection.attr("contenteditable", "true");
+  newSaveDiv.attr("class", "col-1 save align-middle pt-4 pl-4 rounded-right")
+  newSaveDiv.attr("dataId", hoursArr[i])
+  var newIcon = $("<i>");
+  newIcon.attr("class", "far fa-save fa-2x m-auto d-block")
+
+  newTimeDiv.text(hoursArr[i]);
+  newEventSection.text("");
 
 
+  newRowDiv.append(newTimeDiv, newEventSection, newSaveDiv);
+  newSaveDiv.append(newIcon);
 
-           var  newTimeDiv = $("<div>");
-           var  newEventDiv = $("<div>");
-            var newSaveDiv = $("<div>");
-                newTimeDiv.attr("class", "col-1 time justify-center rounded-left pt-4");
-                newTimeDiv.attr("data" , hoursArr[i])
-                newEventDiv.attr("class", "col-10 event justify-center border-left border-right border-dark pt-4");
-                newEventDiv.attr("data" , hoursArr[i])
-                newEventDiv.attr("contenteditable", "true");
-                newSaveDiv.attr("class", "col-1 save align-middle pt-4 pl-4 rounded-right")
-                newSaveDiv.attr("data" , hoursArr[i])
-            var    newIcon = $("<i>");
-                newIcon.attr("class", "far fa-save fa-2x m-auto d-block")
-                
-                newTimeDiv.text(hoursArr[i]);
-                newEventDiv.text("Placeholder");
-            
-              
-                newRowDiv.append(newTimeDiv, newEventDiv, newSaveDiv);
-                newSaveDiv.append(newIcon);
 
+  var oldInfo = JSON.parse(localStorage.getItem("events"));
+  console.log(oldInfo);
+  for (let j = 0; j < oldInfo.length; j++) {
+    if (oldInfo[j].dataId === hoursArr[i]) {
+      newEventSection.text(oldInfo[j].text);
     }
+  }
+  console.log(currentHour);
+  var timeTest;
+  if (hoursArr[i] < 8) {
+    timeTest = (parseInt(hoursArr[i]) + 12);
+  }
+  else {
+    timeTest = hoursArr[i];
+  }
+
+  if (currentHour > timeTest) {
+    newEventSection.addClass("past");
+  }
+  else if (currentHour === timeTest) {
+    newEventSection.addClass("present")
+  }
+
+  else {
+    newEventSection.addClass("future")
+  }
 
 
-   $(document).on("click", ".save", function(event){
-    var newText = $(this).siblings(".text").text();
-    console.log(newText);
-      console.log("save button clicked in the Save event function");
-   });
+}
+
+
+
+
+$(document).on("click", ".save", function (event) {
+  var newText = $(this).siblings("section").text();
+  console.log(newText);
+  console.log("save button clicked in the Save event function");
+  newEvent = {
+    dataId: $(this).attr("dataId"),
+    text: newText
+  }
+  let newEvents = [];
+  let oldEvents = JSON.parse(localStorage.getItem("events"));
+  if (oldEvents !== null) {
+    newEvents = oldEvents;
+  }
+  newEvents.push(newEvent);
+  storageArr = JSON.stringify(newEvents);
+  localStorage.setItem("events", storageArr);
+});
 
 
     // function saveEvent(event) {
@@ -51,8 +92,7 @@ var scheduleArr = [];
     //   console.log("save button clicked in the Save event function");
 
     // }
-    
- 
 
 
-    
+
+
