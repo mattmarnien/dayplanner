eadbcb26501d03d1b05eb768d7296076aa8a9f54
mattmarnien <mgm215@gmail.com>
2020-02-14 21:23:08 -0500

    Updated reademe, script formatting, and text color

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # dayplanner
 
-This app is a single-day planner broken down by hours from 8 am to 6 pm. The app allows users to edit the text in the main column to enter things to do. The right column will save the event entered in the matching row. The app also checks the time against the schedule and styles the page to show time blocks that have passed, the current time block, and future events.
+This app is a single-day planner broken down by hours from 8 am to 6 pm. The app allows users to edit the text in the main column to enter things to do. The right column will save the event entered in the matching row. The events are stores in local storage and will persist through refreshes.
+ The app also checks the time against the schedule and styles the page to show time blocks that have passed, the current time block, and future events. 
+
+# Deployed Link
+https://mattmarnien.github.io/dayplanner/
+
+# Screenshot
+![images/dayPlanner.PNG]
--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-
 var timeNow = $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm"));
-
 var saveButtons = $(".save");
 var hoursArr = ["8", "9", "10", "11", "12", "1", "2", "3", "4", "5", "6"];
 var scheduleArr = [];
@@ -9,28 +7,22 @@ var currentHour = moment().hour();
 for (let i = 0; i < hoursArr.length; i++) {
   var newRowDiv = $("<div>");
   newRowDiv.attr("class", "row w-100");
-  $("#body").append(newRowDiv);
-  
-   var newTimeDiv = $("<div>");
+  $("#body").append(newRowDiv);  
+  var newTimeDiv = $("<div>");
   var newEventSection = $("<section>");
   var newSaveDiv = $("<div>");
+  var newIcon = $("<i>");
   newTimeDiv.attr("class", "col-1 time justify-center rounded-left pt-4");
-  newTimeDiv.attr("data", hoursArr[i])
   newEventSection.attr("class", "col-10 event justify-center border-left border-right border-dark pt-4");
   newEventSection.attr("dataId", hoursArr[i])
   newEventSection.attr("contenteditable", "true");
-  newSaveDiv.attr("class", "col-1 save align-middle pt-4 pl-4 rounded-right")
-  newSaveDiv.attr("dataId", hoursArr[i])
-  var newIcon = $("<i>");
-  newIcon.attr("class", "far fa-save fa-2x m-auto d-block")
-
+  newSaveDiv.attr("class", "col-1 save align-middle pt-4 pl-4 rounded-right");
+  newSaveDiv.attr("dataId", hoursArr[i]); 
+  newIcon.attr("class", "far fa-save fa-2x m-auto d-block");
   newTimeDiv.text(hoursArr[i]);
   newEventSection.text("");
-
-
   newRowDiv.append(newTimeDiv, newEventSection, newSaveDiv);
   newSaveDiv.append(newIcon);
-
 
   var oldInfo = JSON.parse(localStorage.getItem("events"));
   if (oldInfo !== null){
@@ -40,7 +32,6 @@ for (let i = 0; i < hoursArr.length; i++) {
     }
   }
 }
-  console.log(currentHour);
   var timeTest;
   if (hoursArr[i] < 8) {
     timeTest = (parseInt(hoursArr[i]) + 12);
@@ -48,28 +39,19 @@ for (let i = 0; i < hoursArr.length; i++) {
   else {
     timeTest = hoursArr[i];
   }
-
   if (currentHour > timeTest) {
     newEventSection.addClass("past");
   }
   else if (currentHour === timeTest) {
     newEventSection.addClass("present")
   }
-
   else {
     newEventSection.addClass("future")
   }
-
-
 }
-
-
-
 
 $(document).on("click", ".save", function (event) {
   var newText = $(this).siblings("section").text();
-  console.log(newText);
-  console.log("save button clicked in the Save event function");
   newEvent = {
     dataId: $(this).attr("dataId"),
     text: newText
@@ -84,15 +66,6 @@ $(document).on("click", ".save", function (event) {
   localStorage.setItem("events", storageArr);
 });
 
-
-    // function saveEvent(event) {
-
-
-    // var newText = $(this).siblings();
-    // console.log(newText);
-    //   console.log("save button clicked in the Save event function");
-
-    // }
 
 
 
